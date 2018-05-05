52c145d704d834b3ff2ee8d13ecc3a20e12d7839
cyronquillo <cbronquillo@up.edu.ph>
2018-05-06 00:45:00 +0800

    fixed inputting letter catching and empty cell catching;

--- CellKeyListener.java
+++ CellKeyListener.java
@@ -28,18 +28,26 @@ class CellKeyListener implements KeyListener {
         char c = e.getKeyChar();
         JTextField textField = (JTextField) e.getSource();
         String regex = textField.getText().replaceAll("[^\\d]", "");
-
-        int num = Integer.parseInt(regex);
-        if(num < 1) num = 1;
-        if(num > this.puzzle.size) num = this.puzzle.size;
-        
-        if((c < '0' || c > '9') && c != KeyEvent.VK_BACK_SPACE){
-            e.consume();
+        if(!regex.matches("\\d+")){
+            this.puzzle.board[this.row][this.col].top_of_stack = 0;
+            textField.setText("");   
         } else{
-            this.puzzle.board[this.row][this.col].top_of_stack = num;
-            this.puzzle.outputBoard();
-            textField.setText("" + num);   
-        }         
+            int num = Integer.parseInt(regex);
+            if(num < 1) num = 0;
+            if(num > this.puzzle.size) num = this.puzzle.size;
+            if((c < '0' || c > '9') && c != KeyEvent.VK_BACK_SPACE){
+                e.consume();
+            } else{
+                this.puzzle.board[this.row][this.col].top_of_stack = num;
+                this.puzzle.outputBoard();
+                if(num != 0){
+                    textField.setText("" + num);   
+                } else{
+                    textField.setText("");
+                }
+            }         
+
+        }
 
     }
 }
